8cef57245424c26456fc5d67bb53d01b68ed14e8
Viacheslav Smirnov <viacheslav.smirnov@jetbrains.com>
2025-09-17 17:08:47 +0200

    Add test resource for merged call tree sampling in YourKit converter

---
+++ yourkit-convertor/src/main/java/com/jetbrains/perfinsight/yk/calculate/Calculator.java
@@ -0,0 +1,7 @@
+package com.jetbrains.perfinsight.yk.calculate;
+
+import com.jetbrains.perfinsight.yk.model.View;
+
+public interface Calculator {
+    View doCacculate(View view);
+}
---
+++ yourkit-convertor/src/main/java/com/jetbrains/perfinsight/yk/calculate/CountingFieldsCalculator.java
@@ -0,0 +1,13 @@
+package com.jetbrains.perfinsight.yk.calculate;
+
+import com.jetbrains.perfinsight.yk.model.View;
+
+/**
+ * calculate the field count_multiplicator_percent for the nodes.
+ */
+public class CountingFieldsCalculator implements Calculator{
+    @Override
+    public View doCacculate(View view) {
+        return null;
+    }
+}
