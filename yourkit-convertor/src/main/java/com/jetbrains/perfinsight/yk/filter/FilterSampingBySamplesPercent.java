2645e978d77d6dd7c1ea703a281e13ddc953d4d0
Viacheslav Smirnov <viacheslav.smirnov@jetbrains.com>
2025-09-17 16:47:35 +0200

    Add JavaDoc for `FilterSampingBySamplesPercent` class detailing sample filtering logic based on percentage threshold

--- yourkit-convertor/src/main/java/com/jetbrains/perfinsight/yk/filter/FilterSampingBySamplesPercent.java
+++ yourkit-convertor/src/main/java/com/jetbrains/perfinsight/yk/filter/FilterSampingBySamplesPercent.java
@@ -2,6 +2,20 @@ package com.jetbrains.perfinsight.yk.filter;
 
 import com.jetbrains.perfinsight.yk.model.View;
 
+/**
+ * The class get the samples field from the root node, and calculate the percentage of samples for all child nodes.
+ * If the percentage of samples is greater than the minPercent, the node is included in the result.
+ *
+ * For example, the root node has 100 samples, and the child node has 50 samples.
+ * The percentage of samples for the child node is 50%.
+ * The minPercent is 50%.
+ * The child node is included in the result.
+ *
+ * For example, the root node has 100 samples, and the child node has 20 samples.
+ * The percentage of samples for the child node is 20%.
+ * The minPercent is 50%.
+ * The child node is excluded from the result.
+ */
 public class FilterSampingBySamplesPercent implements Filter {
     @Override
     public View doFilter(View view, Double minPercent) {
